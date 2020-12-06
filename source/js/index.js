18b31c5c72ec51c665455d8998c024619bdb9243
Astemir <amaremkulov96@gmail.com>
2020-12-06 14:37:00 +0300

    Исправляет отступы и фоны, добавляет адаптивную графику

---
+++ source/js/index.js
@@ -0,0 +1,25 @@
+// const togglePageHeader = document.querySelector('.page-header__toggle');
+// const navMain = document.querySelector('.main-nav');
+
+// togglePageHeader.classList.remove('page-header__toggle--nojs');
+// navMain.classList.remove('main-nav--nojs');
+
+// togglePageHeader.addEventListener('click', function() {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--open');
+//   } else {
+//     if (navMain.classList.contains('main-nav--open')) {
+//       navMain.classList.add('main-nav--open');
+//       navMain.classList.remove('main-nav--closed');
+//     }
+//   }
+
+//   if (togglePageHeader.classList.contains('page-header__toggle--closed')) {
+//     togglePageHeader.classList.remove('page-header__toggle--closed');
+//     togglePageHeader.classList.add('page-header__toggle--open');
+//   } else {
+//     togglePageHeader.classList.add('page-header__toggle--open');
+//     togglePageHeader.classList.remove('page-header__toggle--closed');
+//   }
+// });
