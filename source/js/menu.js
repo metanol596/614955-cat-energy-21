6dc0e94c8cef2dbc087c77b8e24b14855aff411b
Astemir <amaremkulov96@gmail.com>
2020-12-28 19:04:40 +0300

    ИСправляет отступы, сетку, реализует меню в мобильной версии при отключённом js

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,9 @@
+const headerMain = document.querySelector('.page-header')
 const navToggle = document.querySelector('.page-header__toggle');
 const navMain = document.querySelector('.main-nav');
+
+headerMain.classList.remove('page-header--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navToggle.classList.contains('page-header__toggle--closed')) {
@@ -8,10 +12,10 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--opened');
   } else {
     if (navToggle.classList.contains('page-header__toggle--opened')) {
-      navToggle.classList.add('page-header__toggle--closed');
       navToggle.classList.remove('page-header__toggle--opened');
-      navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
+      navToggle.classList.add('page-header__toggle--closed');
+      navMain.classList.add('main-nav--closed');
     }
   }
 })
